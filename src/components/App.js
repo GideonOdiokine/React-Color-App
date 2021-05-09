211ed817f0ebe5d68715aa6a6a0a5f354b315466
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-05-09 11:20:58 +0100

    created a react app and  added a palette component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import {Route,Switch,NavLink} from 'react-router-dom';
+
+import './App.css'
+import Palette from './Palette';
+import seedColors from './seedColors'
+
+ class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Palette {...seedColors[4]} />
+            </div>
+        )
+    }
+}
+
+
+export default App;
