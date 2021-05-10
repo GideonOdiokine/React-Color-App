628efaaff60e462e1ee7b232f8689b04b8ee61ce
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-05-10 15:39:09 +0100

    style colorbox component

---
+++ src/components/ColorBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+ class ColorBox extends Component {
+    render() {
+        const {name, background} = this.props;
+        return (
+            <div style={{background}} className="ColorBox">
+                <div className="copy-container">
+                    <div className="box-content">
+                         <span className="color-title">{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">More</span>
+            </div>
+        )
+    }
+}
+
+export default ColorBox;
