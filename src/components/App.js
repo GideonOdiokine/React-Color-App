e971f1c65b41bc190341c8b67a34fb0370068847
GideonOdiokine <gidysmartodiokineh>
2021-06-24 20:14:50 +0100

    added router to my app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-// import {Route,Switch,NavLink} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import './App.css'
-import Palette from './Palette';
-import seedColors from './seedColors'
-import {generatePalette} from './colorHelpers'
+import "./App.css";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+import { Router } from "react-router";
 
- class App extends Component {
-    render() {
-        // console.log(generatePalette(seedColors[4]))
-        return (
-            <div className="App">
-                <Palette palette={generatePalette(seedColors[1])} />
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    // console.log(generatePalette(seedColors[4]))
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h2>Palette List Goes Here</h2>} />
+        <Route exact path="/palette/:id" render={() => <h2>Palette Id</h2>} />
+      </Switch>
+      //   <div className="App">
+      //     <Palette palette={generatePalette(seedColors[1])} />
+      //   </div>
+    );
+  }
 }
-
 
 export default App;
