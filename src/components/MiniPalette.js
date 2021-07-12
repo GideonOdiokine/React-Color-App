4add392e2ccea2e273a6730e443028e5c52ae534
GideonOdiokine <gidysmartodiokineh>
2021-07-12 02:53:10 +0100

    added my minipalette component

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -3,18 +3,42 @@ import { withStyles } from "@material-ui/styles";
 
 const styles = {
   root: {
-    backgroundColor: "purple",
-    border: "1px solid red",
+    backgroundColor: "white",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    border: "1px solid black",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  colors: {
+    backgroundColor: "grey",
+  },
+  title: {
+    display: "flex",
+    fontFamily: "sans-serif",
+    justifyContent: "space-between",
+    alignItem: "center",
+    color: "black",
+    paddingTop: "0.3rem",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
   },
 };
 const MiniPalette = (props) => {
-  const { classes, name, emoji } = props;
+  const { classes, paletteName, emoji } = props;
   console.log(classes);
   return (
     <div className={classes.root}>
       <div className={classes.colors}></div>
       <h5 className={classes.title}>
-        {name} <span>{emoji}</span>
+        {paletteName} <span>{emoji}</span>
       </h5>
     </div>
   );
--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -7,10 +7,9 @@ class PaletteList extends Component {
     const { palettes } = this.props;
     return (
       <div>
-
-        <h1>React Colors</h1>
+        <h1 style={{ fontFamily: "sans-serif" }}>React Colors</h1>
         {palettes.map((palette) => (
-          <MiniPalette {...palette} />
+          <MiniPalette {...palette} key={palette.id} />
         ))}
       </div>
     );
