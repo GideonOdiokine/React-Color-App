07d2b51a297e3bea9fa2b97f7e4038ceb998d5ea
GideonOdiokine <gidysmartodiokineh>
2021-07-15 23:50:38 +0100

    added navbar to singleComponent

--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = { level: 500, colorFormat: "hex", open: false };
     console.log(this._shades);
   }
   gatherShades = (palette, colorToFilterBy) => {
@@ -18,18 +20,36 @@ class SingleColorPalette extends Component {
     }
     return shades.slice(1);
   };
+
+  changeLevel = (newLevel) => {
+    this.setState({ level: newLevel });
+  };
+
+  handleColorChange = (e) => {
+    this.setState({ colorFormat: e.target.value, open: true });
+    setTimeout(() => {
+      this.setState({ open: false });
+    }, 4000);
+  };
+
   render() {
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
-        key={color.id}
+        key={color[this.state.colorFormat]}
         name={color.name}
-        background={color.hex}
+        background={color[this.state.colorFormat]}
         showLink={false}
       />
     ));
     return (
       <div className="Palette">
-        <h3>SingleColorPalette</h3>
+        <Navbar
+          handleChangeFormat={this.handleColorChange}
+          colorFormat={this.state.colorFormat}
+          open={this.state.open}
+          closeMessage={this.closeMessage}
+        />
+        {/* <h3>SingleColorPalette</h3> */}
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
