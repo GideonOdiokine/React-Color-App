75f36a6889c2b80a307f3cd28ca5bed40332b2d1
GideonOdiokine <gidysmartodiokineh>
2021-07-21 19:59:19 +0100

    NewpaletteForm component added

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 class App extends Component {
   findPalette(id) {
@@ -17,7 +18,7 @@ class App extends Component {
     // console.log(generatePalette(seedColors[4]));
     return (
       <Switch>
-        <Route exact path="/palette/new" render={() => <h2>New Form </h2>} />
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/palette/:paletteId/:colorId"
